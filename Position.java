e6270f92c5fd875b3a99b934e9f280a11aa21912
SteinarSi <56594043+SteinarSi@users.noreply.github.com>
2020-01-28 15:04:11 +0100

    Add files via upload

--- Position.java
+++ Position.java
@@ -1,39 +1,53 @@
 package Chessbot2;
 
-import java.util.Dictionary;
-
-import static Chessbot2.Chess.directions;
+import javax.imageio.stream.ImageInputStream;
 
 public class Position {
+
+    static int score;
+    static Tuple wc;
+    static Tuple bc;
+    static boolean ep;
+    static boolean kp;
     static String board =
-            "         \n"+
-            "         \n"+
-            " rnbqkbnr\n"+
-            " pppppppp\n"+
-            " ........\n"+
-            " ........\n"+
-            " ........\n"+
-            " ........\n"+
-            " PPPPPPPP\n"+
-            " RNBQKBNR\n"+
-            "         \n"+
-            "         ";
-    public void/*Tuple<Integer, Integer>*/ gen_moves(){
-        for (int i=0; i<board.length(); i++){
+                    "         \n"+
+                    "         \n"+
+                    " rnbqkbnr\n"+
+                    " pppppppp\n"+
+                    " ........\n"+
+                    " ........\n"+
+                    " ........\n"+
+                    " ........\n"+
+                    " PPPPPPPP\n"+
+                    " RNBQKBNR\n"+
+                    "         \n"+
+                    "         ";
+
+    public Position(String board, int score, Tuple wc, Tuple bc, Boolean ep, Boolean kp){
+        this.board = board;
+        this.score = score;
+        this.wc = wc;
+        this.bc = bc;
+        this.ep = ep;
+        this.kp = kp;
+    }
+    public void/*Tuple<Integer, Integer>*/ gen_moves() {
+        // TODO: 28.01.2020 Fiks rokader, passant, yield
+        for (int i = 0; i < board.length(); i++) {
             char p = board.charAt(i);
-            if (!Character.isUpperCase(p)){
+            if (!Character.isUpperCase(p)) {
                 continue;
             }
-            for(int d : Chess.directions.get(board.charAt(i))){
-                for (int a = i+d; true; a+=d){
+            for (int d : Chess.directions.get(p)) {
+                for (int a = i + d; true; a += d) {
                     char q = board.charAt(a);
 
-                    if(Character.isUpperCase(q) || Character.isWhitespace(q)) break;
+                    if (Character.isUpperCase(q) || Character.isWhitespace(q)) break;
 
-                    if(p == 'P') {
+                    if (p == 'P') {
                         if ((d == Chess.N || d == Chess.N * 2) && q != '.') break;
-                        if (d==Chess.N*2 && (i<Chess.A1+Chess.N || board.charAt(Chess.N+i)!= '.')) break;
-                        if ((d==Chess.N + Chess.E || d== Chess.N + Chess.W) && q == '.') break;
+                        if (d == Chess.N * 2 && (i < Chess.A1 + Chess.N || board.charAt(Chess.N + i) != '.')) break;
+                        if ((d == Chess.N + Chess.E || d == Chess.N + Chess.W) && q == '.') break;
                     }
                     System.out.println(i + " til " + a);
                     if (p == 'P' || p == 'N' || p == 'K' || Character.isLowerCase(q)) break;
@@ -41,13 +55,27 @@ public class Position {
             }
         }
     }
-
-    /*
     public Position rotate(){
-        char[] temp = board.toCharArray();
+        char[] temp2 = board.toCharArray();
+        System.out.println(temp2);
+        for(int i=0; i<temp2.length/2; i++){
+            int temp = temp2[i];
+            temp2[i] = temp2[temp2.length -i -1];
+            temp2[temp2.length -i -1] = (char) temp;
+        } // TODO: 28.01.2020 Fiks at case blir endret
+        return new Position(reverseCase(temp2), this.score, this.wc, this.bc, this.ep, this.kp);
     }
-
-    */
-
+    public static String reverseCase(char[] chars) {
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (Character.isUpperCase(c)) {
+                chars[i] = Character.toLowerCase(c);
+            }
+            else if (Character.isLowerCase(c)) {
+                chars[i] = Character.toUpperCase(c);
+            }
+        }
+        return new String(chars);
+    }
 }
 
