ee6ff7375fec39c6d53b954aeb943e8e201abc8b
SteinarSi <56594043+SteinarSi@users.noreply.github.com>
2020-01-28 15:47:18 +0100

    Add files via upload

--- Chess.java
+++ Chess.java
@@ -24,28 +24,32 @@ public class Chess {
     static int S = 10;
     static int W = -1;
 
-    //Gjørneindekser
+    //Hjørneindekser
     static int A1 =  91;
     static int H1 = 98;
     static int A8 = 21;
     static int H8 = 28;
-    static Tuple initialWC = new Tuple(true, true);
-    static Tuple initialBC = new Tuple(true, true);
+    static Tuple WC = new Tuple(true, true);
+    static Tuple BC = new Tuple(true, true);
 
     static Dictionary<Character, int []> directions = new Hashtable();
 
     public static void main(String[] args) {
+        //Oppretter en dict med alle himmelretningene hver enkelt brikke kan bevege seg.
         directions.put('P', new int[] {N, N+N, N+W, N+E});
         directions.put('N', new int[] {N+N+W, E+N+E, E+S+E, S+S+E, S+S+W, W+S+W, W+N+W, N+N+W});
         directions.put('B', new int[] {N+E, S+E, S+W, N+W});
         directions.put('R', new int[] {N, E, S, W});
         directions.put('Q', new int[] {N, E, S, W, N+E, S+E, S+W, N+W});
         directions.put('K', new int[] {N, E, S, W, N+E, S+E, S+W, N+W});
-        Position P = new Position(board, 0, initialWC, initialBC, true, true);
-        P.gen_moves();
+        Position P = new Position(board, 0, WC, BC, true, true);
+        System.out.println(board);
+        System.out.println(P.rotate().board);
+
 
     }
     public static int parse(String c){
+        // Konverterer poisjonen på brettet fra bokstav+tall til kun tall. f. eks. e2 blir 85.
         int fil = (int) c.charAt(0) - 'a';
         int rank = c.charAt(1) - '1';
         return A1 + fil - 10*rank;
--- Position.java
+++ Position.java
@@ -9,19 +9,7 @@ public class Position {
     static Tuple bc;
     static boolean ep;
     static boolean kp;
-    static String board =
-                    "         \n"+
-                    "         \n"+
-                    " rnbqkbnr\n"+
-                    " pppppppp\n"+
-                    " ........\n"+
-                    " ........\n"+
-                    " ........\n"+
-                    " ........\n"+
-                    " PPPPPPPP\n"+
-                    " RNBQKBNR\n"+
-                    "         \n"+
-                    "         ";
+    static String board;
 
     public Position(String board, int score, Tuple wc, Tuple bc, Boolean ep, Boolean kp){
         this.board = board;
@@ -33,6 +21,10 @@ public class Position {
     }
     public void/*Tuple<Integer, Integer>*/ gen_moves() {
         // TODO: 28.01.2020 Fiks rokader, passant, yield
+        /* Går igjennom alle brikkene til spilleren som skal flytte, og finner hvilke lovlige trekk brikken har.
+        Nå bare printer den trekket, men planen er å få den til å kontinuerlig sende disse trekkene til Searcher(),
+        så den kan vurdere trekk samtidig som den finner nye trekk.
+         */
         for (int i = 0; i < board.length(); i++) {
             char p = board.charAt(i);
             if (!Character.isUpperCase(p)) {
@@ -56,16 +48,19 @@ public class Position {
         }
     }
     public Position rotate(){
+        /* Flipper brettet helt rundt, og inverterer casen til alle bokstavene.
+         */
         char[] temp2 = board.toCharArray();
-        System.out.println(temp2);
         for(int i=0; i<temp2.length/2; i++){
             int temp = temp2[i];
             temp2[i] = temp2[temp2.length -i -1];
             temp2[temp2.length -i -1] = (char) temp;
-        } // TODO: 28.01.2020 Fiks at case blir endret
+        }
         return new Position(reverseCase(temp2), this.score, this.wc, this.bc, this.ep, this.kp);
     }
     public static String reverseCase(char[] chars) {
+        /* Går igjennom hele listen med bokstaver, og inverterer casen til alle bokstavene.
+         */
         for (int i = 0; i < chars.length; i++) {
             char c = chars[i];
             if (Character.isUpperCase(c)) {
