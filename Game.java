f18d66045f28bc7492c4751d6e89b588b7a1cb87
SteinarSi <56594043+SteinarSi@users.noreply.github.com>
2020-02-17 11:59:19 +0100

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,6 +1,6 @@
 package Chessbot2;
 
-import javafx.geometry.Pos;
+//import javafx.geometry.Pos;
 
 import java.util.ArrayList;
 
@@ -9,10 +9,12 @@ import static Chessbot2.Chess.*;
 public class Game {
 
     ArrayList<Position> madeMoves;
+    Position currentBoard;
 
     public Game(){
         madeMoves = new ArrayList<>();
-        madeMoves.add(new Position(board, 0, WC, BC, 0, true));
+        Position currentBoard = new Position(board, 0, WC, BC, 0, true);
+        madeMoves.add(currentBoard);
     }
 
     public String getPosition() {
@@ -20,23 +22,36 @@ public class Game {
     }
 
     public void back() {
-        madeMoves.remove(madeMoves.size()-1);
+        /*Sletter de to siste brettene. Hvis spilleren gjør en tabbe,
+        og botten utnytter det, er det meningsløst å kun slette de siste brettet,
+        da bare gjør botten det samme en gang til. Derfor sletter vi to brett.
+        */
+        if(madeMoves.size() >= 2) {
+            madeMoves.remove(madeMoves.size() - 1);
+            madeMoves.remove(madeMoves.size() - 1);
+            currentBoard = madeMoves.get(-1);
+        }else System.out.println("Can't go further back!");
     }
 
     public boolean move(String command) {
-
-        if (!IsAMove(command)) System.err.println("Ugyldig input");
-        else {
+        /* Forsøker å gjøre spillerens trekk.
+        Tar hvilken som helst streng som input,
+        og forsøker å oversette det til en tuppel før den sender det til Position og gjør trekket.
+        Returnerer true/false, basert på om den klarte å gjøre noe.
+        f. eks " \n  e2    e4sd jknsd bjsd\n" Blir oversatt til Tuple(85, 65), og returnerer true.
+        f. eks "ijnsdfhjb e2e4 sdkjn sdfjl sd jsd  sdjnsd " er uforståelig og returnerer false uten å bli gjort.
+         */
+        if(IsAMove(command)) {
             Tuple<Integer, Integer> com = parse(command);
-
-            Position last = madeMoves.get(madeMoves.size()-1);
-
-            if (last.gen_player_moves(com)) {
-                madeMoves.add(last.move(com));
-                return true;
-            }
-        }
-
+            if (currentBoard.gen_player_moves(com)) {
+                if (currentBoard.checkCheck(com)) {
+                    currentBoard.move(com);
+                    currentBoard = currentBoard.rotate();
+                    madeMoves.add(currentBoard);
+                    return true;
+                } else System.err.println("You're in check!");
+            } else System.err.println("Not a legal move!");
+        } else System.err.println("Try typing a move on the format 'letter number letter number'");
         return false;
     }
 }
