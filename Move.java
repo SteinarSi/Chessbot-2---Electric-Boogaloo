a4efa7e4b7dd784bc91815a0891a661fd4a54400
SteinarSi <56594043+SteinarSi@users.noreply.github.com>
2020-02-20 16:38:08 +0100

    Add files via upload
    Har gjort Move generisk, og både move og Tuple har nå implementert iMove.

--- Move.java
+++ Move.java
@@ -1,36 +1,31 @@
 package Chessbot2;
 
-public class Move
+public class Move<X, Y> implements iMove<X, Y>
 {
-    int x;
-    int y;
+    X x;
+    Y y;
     int stabilityIndex;
     int weight;
 
-    public Move(int x, int y)
+    public Move(X x, Y y)
     {
         this.x = x;
         this.y = y;
     }
-
-    public Move(int x, int y, int stabIndex, int weight)
+    public Move(X x, Y y, int stabIndex, int weight)
     {
         this.x = x;
         this.y = y;
         this.stabilityIndex = stabIndex;
         this.weight = weight;
     }
+    public X getX() { return this.x; }
+    public Y getY() { return this.y; }
 
-    public int getX() { return this.x; }
-    public int getY() { return this.y; }
+    public void setX(X x) { this.x = x; }
+    public void setY(Y y) { this.y = y; }
 
-    public void setX(int x) { this.x = x; }
-    public void setY(int y) { this.y = y; }
+    public String toString() {return "(" + this.x + ", " + this.y + ")"; }
 
-    public String toString(){
-        return "(" + this.x + ", " + this.y + ")";
-    }
-    public boolean equals(Move obj){
-        return (this.x == obj.getX() && this.y == obj.getY());
-    }
+    public boolean equals(Move obj){ return (this.x == obj.getX() && this.y == obj.getY()); }
 }
