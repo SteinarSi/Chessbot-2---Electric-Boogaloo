46a050da6fea6caae1cc3f5e44b16dca3656a69b
Sander Wiig <sander.wiig@student.uib.no>
2020-02-20 16:29:50 +0100

    Move.java

---
+++ Chessbot2/Planning_etc/Searching stuff/Move.java
@@ -0,0 +1,37 @@
+/**
+ * IMove
+ */
+public class Move 
+{
+    int x;
+    int y;
+    int stabilityIndex;
+    int weight;
+
+    public Move(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Move(int x, int y, int stabIndex, int weight) 
+    {
+        this.x = x;
+        this.y = y;
+        this.stabilityIndex = stabIndex;
+        this.weight = weight;
+    }
+
+    public int getX() { return this.x; }
+    public int getY() { return this.y; }
+
+    public void setX(int x) { this.x = x; }
+    public void setY(int y) { this.y = y; }
+
+    public String toString(){
+        return "(" + this.x + ", " + this.y + ")";
+    }
+    public boolean equals(Move obj){
+        return (this.x == obj.getX() && this.y == obj.getY());
+    }
+}
